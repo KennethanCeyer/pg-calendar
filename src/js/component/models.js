4c2a33fe4bf4b9586de23a5993acbc902be4f005
Han SungMin <kenneth.ceyer@navercorp.com>
2018-07-30 18:55:56 +0900

    update npm version to 1.4.30

--- src/js/component/models.js
+++ src/js/component/models.js
@@ -1,7 +1,7 @@
 define([], () => {
   const models = {
     name: 'pignoseCalendar',
-    version: '1.4.29',
+    version: '1.4.30',
     preference: {
       supports: {
         themes: ['light', 'dark', 'blue']
