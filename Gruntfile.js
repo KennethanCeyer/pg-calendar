afde6d8fe418032eb5401eac54ef652a47654f98
Han SungMin <kenneth.ceyer@navercorp.com>
2019-07-11 13:44:26 +0900

    WIP: update version to 1.4.31

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function (grunt) {
     babel: {
       options: {
         sourceMap: true,
-        presets: ['env']
+        presets: ['@babel/preset-env']
       },
       dist: {
         files: [
